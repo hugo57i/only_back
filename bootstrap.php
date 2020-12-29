806e0973159d56427f495f006c14fafb5ceb6320
Hugo Zigha <hugo.zigha@etu.unistra.fr>
2020-12-29 15:26:39 +0100

    test heroku

--- bootstrap.php
+++ bootstrap.php
@@ -10,12 +10,12 @@ $isDevMode = true;
 $config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
 
 $conn = array(
-    'host' => "localhost",
-    'driver' => 'pdo_mysql',
-    'user' => "root",
-    'password' => "",
-    'dbname' => "e_commerce",
-    'port' => 3306,
+    'host' => "ec2-3-248-4-172.eu-west-1.compute.amazonaws.com",
+    'driver' => 'pdo_pgsql',
+    'user' => "wugyddtxfojmup",
+    'password' => "9ea5a8a516797fbb57646484c65d60c5f2a610b689668c347f60c29ac3144eb6",
+    'dbname' => "d3a3u5os1bgi6k",
+    'port' => 5432,
 );
 
 $entityManager = EntityManager::create($conn, $config);
